c91592f38db4790607d6776f3e7a3553abdcefb7
YaphetKG <kebedey@renci.org>
2020-10-23 10:50:05 -0400

    Moving heartbeat logic to automat

--- main.sh
+++ main.sh
@@ -1,40 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # set environment variables from .env
 set -a
 source .env
 set +a
 
-function webServer {
-    echo "Starting web server ... "
-    if [ "$MODE" == "deploy" ]; then
-        gunicorn PLATER.services.app:APP -b ${WEB_HOST}:${WEB_PORT} -w 4 -k uvicorn.workers.UvicornWorker
-    else
-        uvicorn PLATER.services.app:APP --host ${WEB_HOST} --port ${WEB_PORT} --reload
-    fi
-}
-
-function heartbeat {
-    if [[ ! -z "${AUTOMAT_HOST}" ]]; then
-        echo "Contacting Automat"
-        testEndpoint=${PLATER_SERVICE_ADDRESS}:${WEB_PORT}/predicates
-        response=$(curl --write-out %{http_code} --silent --output /dev/null ${testEndpoint})
-        until [ $response = "200" ]; do
-            response=$(curl --write-out %{http_code} --silent --output /dev/null ${testEndpoint})
-            echo "Plater not ready sleeping... "
-            sleep 1
-        done
-        echo "Plater ready starting heartbeat ..."
-        python PLATER/services/heartbeat.py -a ${AUTOMAT_HOST}
-    fi
-    $*
-}
+if [ "$MODE" == "deploy" ]; then
+    gunicorn PLATER.services.app:APP -b ${WEB_HOST}:${WEB_PORT} -w 4 -k uvicorn.workers.UvicornWorker
+else
+    uvicorn PLATER.services.app:APP --host ${WEB_HOST} --port ${WEB_PORT} --reload
+fi
 
 # Run the web server in the background
-webServer &
-webServerPID=$!
-# Run heartbeat sender in background, only runs if AUTOMAT_SERVER env is provided
-heartbeat &
-heartbeatPID=$!
-# add wait so killing this script would kill the bg processes.
-wait $webServerPID $heartbeatPID
+webServer
