707065b09da9d4fe5130315f342df22728c78fa5
Phil Owen <phillipsowen@outlook.com>
2021-07-09 16:51:44 -0400

    fixing path to data file

--- PLATER/services/util/question.py
+++ PLATER/services/util/question.py
@@ -5,9 +5,10 @@ import time
 import reasoner
 import json
 from reasoner.cypher import get_query
+import os
 
 # load the attrib and value mapping file
-map_data = json.load(open("../../attr_val_map.json"))
+map_data = json.load(open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "attr_val_map.json")))
 
 # set the transpiler attribute mappings
 reasoner.cypher.ATTRIBUTE_TYPES = map_data['attribute_type_map']
