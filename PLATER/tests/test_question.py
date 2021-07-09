3d53d63c5d1650c8893065d809cee2624ae197b6
Phil Owen <phillipsowen@outlook.com>
2021-07-09 15:48:13 -0400

    updating test to cover the new provenance additions

--- PLATER/tests/test_question.py
+++ PLATER/tests/test_question.py
@@ -15,27 +15,41 @@ def test_init():
 
 
 def test_format_attribute():
-    trapi_kg_response = {"knowledge_graph": {"nodes": {"CURIE:1": {"attributes": [{"original_attribute_name": "pub", "attribute_type_id": "CURIE:x"}]}}}}
-    expected_trapi = {"knowledge_graph": {"nodes": {"CURIE:1": {"attributes": [{"attribute_type_id": "CURIE:x", "value_type_id": "biolink:Attribute", "original_attribute_name":"pub"}]}} }}
+    trapi_kg_response = {"knowledge_graph": {"nodes": {"CURIE:1": {"attributes": [
+        {"original_attribute_name": "pub", "attribute_type_id": "CURIE:x"},
+        {"original_attribute_name": "biolink:original_knowledge_source", "value": "infores:test"}]
+    }}}}
+
+    expected_trapi = {"knowledge_graph": {"nodes": {"CURIE:1": {"attributes": [
+        {"original_attribute_name": "pub", "attribute_type_id": "CURIE:x", "value_type_id": "biolink:Attribute"},
+        {"original_attribute_name": "biolink:original_knowledge_source", "value": "infores:test", "attribute_type_id": "EDAM:data_0006", "value_type_id": "biolink:InformationResource"},
+        {"attribute_type_id": "biolink:aggregator_knowledge_source", "value": "infores:plater", "value_type_id": "biolink:InformationResource", "original_attribute_name": "biolink:aggregator_knowledge_source"}]
+    }}}}
+
     q = Question(question_json={})
-    # test if attribute_id if provided from neo4j response is preserved
+
+    # test attribute_id if provided from neo4j response is preserved
     # test if value_type is added to default 'biolink:Attribute'
-    assert q.transform_attributes(trapi_kg_response) == expected_trapi
-    expected_trapi = {
-        "knowledge_graph": {
-            "nodes": {
-                "CURIE:1": {
-                    "attributes": [{"attribute_type_id": "EDAM:data_0006", "value": "x", "original_attribute_name": "pub", "value_type_id": "oo"}]
-                }
-            }
-        }
-    }
-    trapi_kg_response = {
-        "knowledge_graph": {"nodes": {"CURIE:1": {"attributes": [{"original_attribute_name": "pub", "value": "x", "value_type_id": "oo"}]}}}
-    }
+    assert q.transform_attributes(trapi_kg_response.copy()) == expected_trapi
+
+    t2_trapi_kg_response = {"knowledge_graph": {"nodes": {"CURIE:1": {"attributes": [
+        {"original_attribute_name": "pub", "value": "x", "value_type_id": "oo", "attribute_type_id": "preserved_attrib"},
+        {"original_attribute_name": "endogenous", "value": "false"},
+        {"original_attribute_name": "equivalent_identifiers", "attribute_type_id": "biolink:same_as", "value": ["some_identifier"]}]
+    }}}}
+
+    t2_expected_trapi = {'knowledge_graph': {'nodes': {'CURIE:1': {'attributes': [
+        {'original_attribute_name': 'pub', 'value': 'x', 'value_type_id': 'biolink:Attribute', 'attribute_type_id': 'preserved_attrib'},
+        {'original_attribute_name': 'endogenous', 'value': 'false', 'value_type_id': 'xsd:boolean', 'attribute_type_id': 'EDAM:data_0006'},
+        {"original_attribute_name": "equivalent_identifiers", "attribute_type_id": "biolink:same_as", "value": ["some_identifier"], 'value_type_id': 'metatype:uriorcurie'},
+        {'attribute_type_id': 'biolink:aggregator_knowledge_source', 'value': 'infores:plater', 'value_type_id': 'biolink:InformationResource', 'original_attribute_name': 'biolink:aggregator_knowledge_source'}]
+    }}}}
+
+    call_data = q.transform_attributes(t2_trapi_kg_response.copy())
+
     # test default attribute to be EDAM:data_0006
     # test if value_type is preserved if in response from neo4j
-    assert q.transform_attributes(trapi_kg_response) == expected_trapi
+    assert call_data == t2_expected_trapi
 
 
 class MOCK_GRAPH_ADAPTER():
