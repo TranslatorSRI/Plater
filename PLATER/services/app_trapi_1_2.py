8a7f865c28167b364de028c46624d037f68511cb
Yaphetkg <yaphetkg@renci.org>
2021-09-20 09:56:20 -0400

    annotate_k_nodes and overlay_k_nodes operations

--- PLATER/services/app_trapi_1_2.py
+++ PLATER/services/app_trapi_1_2.py
@@ -5,6 +5,7 @@ from PLATER.models.models_trapi_1_1 import (MetaKnowledgeGraph, Message, Reasone
 
 from PLATER.services.util.graph_adapter import GraphInterface
 from PLATER.services.util.question import Question
+from PLATER.services.util.overlay import Overlay
 from PLATER.services.util.api_utils import get_graph_interface, construct_open_api_schema, get_example
 
 # Mount open api at /1.2/openapi.json
@@ -28,11 +29,21 @@ async def reasoner_api(
 ):
     """Handle TRAPI request."""
     request_json = request.dict(by_alias=True)
-    workflow = request_json.get('workflow', [{"id": "lookup"}])
-    # @TODO handle workflow
-    question = Question(request_json["message"])
-    response = await question.answer(graph_interface)
-    request_json.update({'message': response, 'workflow': workflow})
+    # default workflow
+    workflow = request_json.get('workflow') or [{"id": "lookup"}]
+    workflows = {wkfl['id']: wkfl for wkfl in workflow}
+    if 'lookup' in workflows:
+        question = Question(request_json["message"])
+        response = await question.answer(graph_interface)
+        request_json.update({'message': response, 'workflow': workflow})
+    elif 'overlay_connect_knodes' in workflows:
+        overlay = Overlay(graph_interface=graph_interface)
+        response = await overlay.connect_k_nodes(request_json['message'])
+        request_json.update({'message': response, 'workflow': workflow})
+    elif 'annotate_nodes' in workflows:
+        overlay = Overlay(graph_interface=graph_interface)
+        response = await overlay.annotate_node(request_json['message'])
+        request_json.update({'message': response, 'workflow': workflow})
     return request_json
 
 
