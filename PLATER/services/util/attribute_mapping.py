383cf4b9519e9673b934eb1b25352fd665d44973
Evan Morris <evandietzmorris@gmail.com>
2024-09-30 20:29:55 -0400

    updating attribute type mapping for new consolidated mapping

--- PLATER/services/util/attribute_mapping.py
+++ PLATER/services/util/attribute_mapping.py
@@ -4,11 +4,8 @@ import os
 # load the attrib and value mapping file
 map_data = json.load(open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "attr_val_map.json")))
 
-# set the value type mappings from the file
-VALUE_TYPES = map_data['value_type_map']
-
 # set the attribute type mappings from the file
 ATTRIBUTE_TYPES = map_data['attribute_type_map']
 
 # attribute skip list
-SKIP_LIST = json.load(open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "skip_attr.json")))
+ATTRIBUTE_SKIP_LIST = json.load(open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "skip_attr.json")))
