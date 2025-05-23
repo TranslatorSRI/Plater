a9912bc5b5efe1cddc965c5b29d14ee2ae27a7f2
Evan Morris <evandietzmorris@gmail.com>
2025-05-23 12:30:40 -0400

    adding some cypher queries

--- deploy/performance.py
+++ deploy/performance.py
@@ -36,6 +36,7 @@ def run_performance_analysis(deployments_to_validate=None, performance_spec=None
                     query_files = query_details["files"]
                     queries = query_details["queries"] if "queries" in query_details else None
                     for q_file in query_files:
+                        #  print(f'For environment: {deployment_env} ({url}) running {q_file}')
                         for performance_query in quick_jsonl_file_iterator(f'./{q_file}'):
                             if not performance_query:
                                 continue
@@ -48,11 +49,20 @@ def run_performance_analysis(deployments_to_validate=None, performance_spec=None
                                 print(f'Sending query {query_name} to {deployment_env}: {plater}, iteration {i+1}')
                                 start_time = time.time()
                                 try:
-                                    trapi_response = send_trapi_query(url,
-                                                                      performance_query,
-                                                                      profile=False,
-                                                                      validate=False)
-                                    num_results = len(trapi_response['message']['results'])
+                                    if query_details["query_type"] == "trapi":
+                                        trapi_response = send_trapi_query(url,
+                                                                          performance_query,
+                                                                          profile=False,
+                                                                          validate=False)
+                                        num_results = len(trapi_response['message']['results'])
+                                    elif query_details["query_type"] == "cypher":
+                                        cypher_query = performance_query["cypher_query"]
+                                        cypher_response = send_cypher_query(url,
+                                                                            cypher_query)
+                                        print(cypher_response)
+                                        num_results = 1
+                                    else:
+                                        print("huh")
                                     # print(trapi_response)
                                     duration = time.time() - start_time
                                     print(f'Got back {num_results} in {duration}.')
@@ -72,16 +82,20 @@ def run_performance_analysis(deployments_to_validate=None, performance_spec=None
 if __name__ == '__main__':
 
     # environments = ['exp', 'dev', 'robokop']
-    environments = ['robokop', 'dev']
+    environments = ['robokop']
 
     performance_spec = {
-         "robokopkg": {"files": ["./performance_queries/robokopkg_performance_queries.jsonl"],
-                       "queries": ["gene_to_chemical_qualifier_10"]}
+        #"robokopkg": {"files": ["./performance_queries/robokopkg_performance_trapi.jsonl"],
+        #              "queries": ["gene_to_chemical_qualifier_1"],
+        #              "query_type": "trapi"},
+        "robokopkg": {"files": ["./performance_queries/robokopkg_performance_cypher.jsonl"],
+                      "queries": ["gene_to_chemical_20"],
+                      "query_type": "cypher"}
         # "hmdb": {"files": ["./performance_queries/hmdb_performance_queries.jsonl"]}
     }
 
     # to run for only certain environments
-    run_performance_analysis(environments, performance_spec, iterations=3)
+    run_performance_analysis(environments, performance_spec, iterations=2)
 
     # or all of them
     # run_performance_analysis()
