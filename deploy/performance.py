71f4c7c36eea8ce62e1fd63d80da5504d3d2869b
Evan Morris <evandietzmorris@gmail.com>
2026-03-12 14:03:02 -0400

    include number of edges and size of response in results

--- deploy/performance.py
+++ deploy/performance.py
@@ -40,10 +40,12 @@ def run_performance_analysis(deployments_to_validate=None, performance_spec=None
                         for performance_query in quick_jsonl_file_iterator(f'./{q_file}'):
                             if not performance_query:
                                 continue
-                            query_name = performance_query.pop('name')
+                            query_name = performance_query.pop('query_name')
                             if queries and query_name not in queries:
                                 continue
                             plater_performance_results[plater][deployment_env][query_name] = {'success_duration': [],
+                                                                                              'num_results': [],
+                                                                                              'response_size_bytes': [],
                                                                                               'errors': []}
                             for i in range(iterations):
                                 print(f'Sending query {query_name} to {deployment_env}: {plater}, iteration {i+1}')
@@ -55,18 +57,22 @@ def run_performance_analysis(deployments_to_validate=None, performance_spec=None
                                                                           profile=False,
                                                                           validate=False)
                                         num_results = len(trapi_response['message']['results'])
+                                        response_size = len(json.dumps(trapi_response, separators=(',', ':')).encode('utf-8'))
                                     elif query_details["query_type"] == "cypher":
                                         cypher_query = performance_query["cypher_query"]
                                         cypher_response = send_cypher_query(url,
                                                                             cypher_query)
                                         # print(cypher_response)
                                         num_results = 1
+                                        response_size = len(json.dumps(cypher_response, separators=(',', ':')).encode('utf-8'))
                                     else:
                                         print("huh")
                                     # print(trapi_response)
                                     duration = time.time() - start_time
-                                    print(f'Got back {num_results} in {duration}.')
+                                    print(f'Got back {num_results} results ({response_size} bytes) in {duration}s.')
                                     plater_performance_results[plater][deployment_env][query_name]['success_duration'].append(duration)
+                                    plater_performance_results[plater][deployment_env][query_name]['num_results'].append(num_results)
+                                    plater_performance_results[plater][deployment_env][query_name]['response_size_bytes'].append(response_size)
                                 except requests.exceptions.HTTPError as e:
                                     duration = time.time() - start_time
                                     print(f'Error occured after {duration} seconds: {e}.')
@@ -75,6 +81,7 @@ def run_performance_analysis(deployments_to_validate=None, performance_spec=None
                             success_durations = plater_performance_results[plater][deployment_env][query_name]['success_duration']
                             average = sum(success_durations) / len(success_durations) if success_durations else "N/A"
                             print(f'Average time for {query_name} to {deployment_env}, {plater}: {average}')
+    os.makedirs('./performance_results', exist_ok=True)
     with open(f'./performance_results/performance_analysis_results_{random.randrange(100000)}.json', 'w') as p_out:
         p_out.write(json.dumps(plater_performance_results, indent=4))
 
@@ -88,8 +95,8 @@ if __name__ == '__main__':
         #"robokopkg": {"files": ["./performance_queries/robokopkg_performance_trapi.jsonl"],
         #              "queries": ["gene_to_chemical_qualifier_1"],
         #              "query_type": "trapi"},
-        "robokopkg": {"files": ["./performance_queries/robokopkg_performance_cypher.jsonl"],
-                      "queries": ["gene_to_chemical_1"],
+        "robokopkg": {"files": ["./performance_queries/robokopkg_neo4j_cypher.jsonl"],
+                      "queries": ["gene_to_chemical_1", "gene_to_chemical_1_no_labels_call"],
                       "query_type": "cypher"}
         # "hmdb": {"files": ["./performance_queries/hmdb_performance_queries.jsonl"]}
     }
