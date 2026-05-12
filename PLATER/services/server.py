73c66e6791916b4b1a3dc0f4ab85603ccadf8ff6
hyi <hongyi@renci.org>
2026-05-12 16:29:06 -0400

    remove insecure=True with older SDK version for testing

--- PLATER/services/server.py
+++ PLATER/services/server.py
@@ -42,8 +42,7 @@ if config.get("OTEL_ENABLED", "False") not in ("false", "False"):
         otlp_host = config.get("JAEGER_HOST", "http://localhost/").rstrip('/')
         otlp_port = config.get("JAEGER_PORT", "4317")
         otlp_endpoint = f'{otlp_host}:{otlp_port}'
-        otlp_insecure = config.get("OTEL_EXPORTER_OTLP_INSECURE", "false").lower() == "true"
-        otlp_exporter = OTLPSpanExporter(endpoint=f'{otlp_endpoint}', insecure=otlp_insecure)
+        otlp_exporter = OTLPSpanExporter(endpoint=f'{otlp_endpoint}')
         processor = BatchSpanProcessor(otlp_exporter)
 
     provider.add_span_processor(processor)
